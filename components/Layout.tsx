bbed28a2a2cc991ce0ac6a0fde92c8617b53db75
Rhodanthe1116 <z0916945857@gmail.com>
2021-06-11 00:45:50 +0800

    fix purge css bug, add linter

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,50 +3,48 @@ import Link from 'next/link'
 import Head from 'next/head'
 
 type Props = {
-    children?: ReactNode
-    title?: string
+  children?: ReactNode
+  title?: string
 }
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-    <div style={{ backgroundColor: 'black', color: 'white' }}>
-        <Head>
-            <title>{title}</title>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta name="theme-color" content="#000000" />
-            <meta name="description" content="A new way to learn Lyrics and Language with Typing!" />
-            <meta name="keywords" content="lyrics language learn typing musix musixmatch spotify"></meta>
-        </Head>
-        <div className="min-h-screen overflow-hidden">
-
-            <header className="p-4 mb-4 flex justify-between items-center bg-gray-900">
-                <div className="flex justify-between">
-                    <Link href="/">
-                        <a className="text-xl font-bold pr-2">Type Lyrics</a>
-                    </Link>
-                    {/* <span>@hwchang</span> */}
-                </div>
-                <nav>
-
-
-                    <Link href="/about" >
-                        <a className="mr-2">About</a>
-                    </Link>
-                    <Link href="/profile">
-                        <a>Profile</a>
-                    </Link>
-                </nav>
-
-            </header>
-
-
-            {children}
-            <footer >
-                {/* <hr /> */}
-
-            </footer>
+  <div style={{ backgroundColor: 'black', color: 'white' }}>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="A new way to learn Lyrics and Language with Typing!"
+      />
+      <meta
+        name="keywords"
+        content="lyrics language learn typing musix musixmatch spotify"
+      ></meta>
+    </Head>
+    <div className="min-h-screen overflow-hidden">
+      <header className="p-4 mb-4 flex justify-between items-center bg-gray-900">
+        <div className="flex justify-between">
+          <Link href="/">
+            <a className="text-xl font-bold pr-2">Type Lyrics</a>
+          </Link>
+          {/* <span>@hwchang</span> */}
         </div>
+        <nav>
+          <Link href="/about">
+            <a className="mr-2">About</a>
+          </Link>
+          <Link href="/profile">
+            <a>Profile</a>
+          </Link>
+        </nav>
+      </header>
+
+      {children}
+      <footer>{/* <hr /> */}</footer>
     </div>
+  </div>
 )
 
 export default Layout
