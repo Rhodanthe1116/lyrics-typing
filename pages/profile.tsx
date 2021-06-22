c503b7d90c5da4ac92c49b4f347cca0c70ea8328
ntubrian <k333hoisi@gmail.com>
2021-06-22 11:43:17 +0800

    The third-time fine-tune

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -69,12 +69,12 @@ const RecordItem = ({ record, loading }: RecordItemProps) => {
 
       <a className={`border-0 border-green-200 md:py-3 md:px-6 py-1 px-3 hover:bg-pink-900 flex justify-between bg-gray-900`}>
         <div>
-          <img className="md:w-28 md:h-28 w-20 h-20 object-cover object-fill" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7af26ce5-5288-4db3-a0f9-bd833b0c6c35/dc1yn5d-6a203811-236c-4ce9-a609-cf4d507de21d.png/v1/fill/w_952,h_839,q_70,strp/great_days_album_cover_by_orochismith_dc1yn5d-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwNiIsInBhdGgiOiJcL2ZcLzdhZjI2Y2U1LTUyODgtNGRiMy1hMGY5LWJkODMzYjBjNmMzNVwvZGMxeW41ZC02YTIwMzgxMS0yMzZjLTRjZTktYTYwOS1jZjRkNTA3ZGUyMWQucG5nIiwid2lkdGgiOiI8PTExNDEifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.QJ7sn-riqwsCp_38xmJI1gwNfVlJ_j8iOJ2aKo-Mtak"></img>
+          <img className="md:w-28 md:h-28 w-16 h-16 object-cover object-fill" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7af26ce5-5288-4db3-a0f9-bd833b0c6c35/dc1yn5d-6a203811-236c-4ce9-a609-cf4d507de21d.png/v1/fill/w_952,h_839,q_70,strp/great_days_album_cover_by_orochismith_dc1yn5d-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAwNiIsInBhdGgiOiJcL2ZcLzdhZjI2Y2U1LTUyODgtNGRiMy1hMGY5LWJkODMzYjBjNmMzNVwvZGMxeW41ZC02YTIwMzgxMS0yMzZjLTRjZTktYTYwOS1jZjRkNTA3ZGUyMWQucG5nIiwid2lkdGgiOiI8PTExNDEifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.QJ7sn-riqwsCp_38xmJI1gwNfVlJ_j8iOJ2aKo-Mtak"></img>
         </div>
 
-        <div className="flex-1 truncate ml-2">
-          <p className="font-bold text-lg  md:text-3xl">Pale Blue</p>
-          <p className="text-gray-400 mt-4 md:mt-8 text-sm md:text-xl">Kenshi Yonezu</p>
+        <div className="flex-1 truncate ml-4 mt-2 md:ml-8">
+          <p className="font-bold text-2xl  md:text-3xl">Pale Blue</p>
+          <p className="font-bold text-gray-400 mt-4 md:mt-8 text-sm md:text-xl">Kenshi Yonezu</p>
         </div>
         <div className="w-auto overflow-hidden text-right">
           <p className="text-red-500 md:text-3xl">{record.result.wpm} CPM</p>
@@ -141,16 +141,16 @@ const ProfilePage = () => {
 
       <div className="">
         <div className="relative">
-            <div className="flex mt-2">
+            <div className="flex mt-8">
               <div className="flex-1 justify-center">
                 <div className="flex items-center justify-center gap-2 min-w-max ml-3">
                   <img className="md:w-48 md:h-48 w-20 h-20 rounded-full" src="https://picsum.photos/500/500" alt="" width="384" height="512"></img>
-                  <h3 className="md:text-2xl text-sm font-bold min-w-max p-2">{user.name}</h3>
+                  <h3 className="md:text-2xl text-base font-bold min-w-max p-2">{user.name}</h3>
                 </div>
               </div>
               <div className="flex-1 text-center transform translate-y-1">
-                <h2 className="text-green-200 font-bold md:text-9xl text-6xl"> {user?.completedTrackIds?.length || 0}</h2>
-                <p> Songs Completed</p>
+                <h2 className="text-green-200 font-bold md:text-9xl text-5xl"> {user?.completedTrackIds?.length || 0}</h2>
+                <p className="font-bold"> Songs Passed</p>
               </div>
             </div>
             
@@ -159,12 +159,12 @@ const ProfilePage = () => {
 
       <div className="m-4 m-8">
         {/* bg-gray-900 p-3 */}
-        <div className="md:mb-8 mb-4">
+        <div className="md:mb-24 mb-16">
          
           {/* <p>{user.id}</p> */}
           <div className="flex">
             <div className="flex-1 ">
-              <p className="md:text-3xl font-semibold text-left pl-6">Average CMP</p>
+              <p className="md:text-3xl font-semibold text-left pl-1">Average CMP</p>
               <div className="md:pt-4 md:text-xl pt-1 grid grid-flow-row grid-cols-2 grid-rows-3 text-center">
                 <div>JP</div>
                 <div>32</div>
@@ -175,7 +175,7 @@ const ProfilePage = () => {
               </div>
             </div>
             <div className="flex-1">
-              <p className="md:text-3xl font-semibold text-left pl-6">Top Artists</p>
+              <p className="md:text-3xl font-semibold text-left pl-1">Top Artists</p>
               <div className="md:pt-4 md:text-xl pt-1 grid grid-flow-row grid-cols-2 grid-rows-3 text-center">
                 <div>A</div>
                 <div>32</div>
